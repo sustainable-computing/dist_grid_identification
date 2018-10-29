492181cf924e7f48e0716ad94580d9c947b86b89
ardakanian <38474012+ardakanian@users.noreply.github.com>
2018-10-28 18:45:32 -0600

    Create connectLoads2Nodes.m

---
+++ connectLoads2Nodes.m
@@ -0,0 +1,27 @@
+function [P,Q] = connectLoads2Nodes(Pn, homesPerNode, startIndex, endIndex, pf)
+% FUNCTION connectLoads2Nodes: creates real and reactive power injection vectors
+
+% INPUT:
+% Pn: a HOMES by simLength matrix containing individual household loads
+% homesPerNode: a vector of size NODES specifying the number of household
+% loads aggregated at each bus. Note that sum(homesPerNode) <= HOMES
+% startIndex: the start index
+% endIndex: the end index
+% pf: predefined power factor at each node injecting power
+
+% OUTPUT:
+% P: total real power injection for each node
+% Q: total reactive power injection for each node
+
+nNodes = length(homesPerNode);
+P = zeros(nNodes, endIndex-startIndex+1);
+% Q = zeros(nNodes, endIndex-startIndex+1);
+ind = 1;
+
+for i=1:nNodes
+    P(i,:) = sum(Pn(ind:ind+homesPerNode(i)-1,startIndex:endIndex));
+    ind = ind + homesPerNode(i);
+end
+
+%pf = pf * (1+.1*(rand-.5));
+Q = sqrt(1/(pf*pf)-1)*P;
