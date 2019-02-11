32a23379cecd534db514b660c6be92f02ba5b42d
Wei Zhou <946958036@qq.com>
2019-02-11 11:18:39 +0800

    Update recoverY.m

--- recoverY.m
+++ recoverY.m
@@ -2,8 +2,8 @@ function [ Y11_est, Y22_est ] = recoverY( W, len_idx, len_nidx )
 
 dimVecY22 = len_idx*(len_idx+1)/2; 
 
-vecY22 = Expan(len_idx) * W(1:dimVecY22);
-vecY11 = Expan(len_nidx)* W(dimVecY22+1:end);
+vecY22 = expan(len_idx) * W(1:dimVecY22);
+vecY11 = expan(len_nidx)* W(dimVecY22+1:end);
 Y22_est = reshape(vecY22(1:len_idx^2), [len_idx, len_idx]);
 Y11_est = reshape(vecY11(1:len_nidx^2), [len_nidx, len_nidx]);
 
